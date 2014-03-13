9bf0427efbcf5f0c0f8f034d50cb32a45bf28079
Mark Janssen <mpc.janssen@gmail.com>
2014-03-13 13:59:43 +0000

    Use robolectric for testing

---
+++ src/test/java/nl/mpcjanssen/simpletask/SimpletaskTest.java
@@ -0,0 +1,13 @@
+package nl.mpcjanssen.simpletask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class SimpletaskTest {
+	@Test
+	public void testInstantiation() {
+		new Simpletask();
+	}
+}
